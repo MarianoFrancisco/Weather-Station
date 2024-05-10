a564c932f6742632fa398bf3a6312f3e30bbeaf9
Peter2510 <pedrogordillo202031683@cunoc.edu.gt>
2024-05-10 10:38:41 -0600

    Agregando graficas

--- app/lib/pages/graphic/body.dart
+++ app/lib/pages/graphic/body.dart
@@ -1,13 +1,110 @@
 import 'package:flutter/material.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+import 'temperature_data.dart';
 
-// @author Mariano Camposeco {@literal (mariano1941@outlook.es)}
 class GraphicReportBody extends StatelessWidget {
-  const GraphicReportBody({super.key});
+  const GraphicReportBody({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Gráfica Information'),
+    final List<TemperatureData> temperatureDataList = [
+      TemperatureData('Cantel', [17, 19, 15, 21, 18]),
+      TemperatureData('Concepcion Chiquirichapa', [17, 19, 15, 21, 18]),
+      TemperatureData('Centro Universitario de Occidente', [23, 25, 28, 22, 26]),
+    ];
+
+    final List<TemperatureData> precipitationDataList = [
+      TemperatureData('Cantel', [10, 15, 8, 12, 9]),
+      TemperatureData('Concepcion Chiquirichapa', [12, 10, 14, 11, 13]),
+      TemperatureData('Centro Universitario de Occidente', [8, 11, 9, 10, 12]),
+    ];
+
+    return ListView(
+      children: [
+        _buildTitle('Temperatura'),
+        ...temperatureDataList.map((data) => createTemperatureChart(data)).toList(),
+        _buildTitle('Precipitación'),
+        ...precipitationDataList.map((data) => createPrecipitationChart(data)).toList(),
+      ],
+    );
+  }
+
+  Widget _buildTitle(String title) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Text(
+        title,
+        style: TextStyle(
+          fontSize: 20,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+
+  Widget createTemperatureChart(TemperatureData data) {
+    final List<Map<String, dynamic>> chartData = [];
+    for (int i = 0; i < data.temperatures.length; i++) {
+      chartData.add({'x': 'Día ${i + 1}', 'y': data.temperatures[i]});
+    }
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: 250,
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        padding: EdgeInsets.all(10),
+        child: SfCartesianChart(
+          primaryXAxis: CategoryAxis(),
+          title: ChartTitle(text: data.location, alignment: ChartAlignment.center),
+          legend: Legend(isVisible: false),
+          series: <ChartSeries>[
+            ColumnSeries<Map<String, dynamic>, String>(
+              dataSource: chartData,
+              xValueMapper: (datum, _) => datum['x'] as String,
+              yValueMapper: (datum, _) => datum['y'] as double,
+              name: 'Temperatura',
+              color: Color.fromARGB(255, 71, 169, 88),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget createPrecipitationChart(TemperatureData data) {
+    final List<Map<String, dynamic>> chartData = [];
+    for (int i = 0; i < data.temperatures.length; i++) {
+      chartData.add({'x': 'Día ${i + 1}', 'y': data.temperatures[i]});
+    }
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        height: 250,
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.grey),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        padding: EdgeInsets.all(10),
+        child: SfCartesianChart(
+          primaryXAxis: CategoryAxis(),
+          title: ChartTitle(text: data.location, alignment: ChartAlignment.center),
+          legend: Legend(isVisible: false),
+          series: <ChartSeries>[
+            ColumnSeries<Map<String, dynamic>, String>(
+              dataSource: chartData,
+              xValueMapper: (datum, _) => datum['x'] as String,
+              yValueMapper: (datum, _) => datum['y'] as double,
+              name: 'Precipitación',
+              color: Color.fromARGB(255, 70, 144, 234),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ app/lib/pages/graphic/temperature_data.dart
@@ -0,0 +1,6 @@
+class TemperatureData {
+  final String location;
+  final List<double> temperatures;
+
+  TemperatureData(this.location, this.temperatures);
+}
